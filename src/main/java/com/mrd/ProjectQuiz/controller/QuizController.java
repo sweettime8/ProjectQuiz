629cca7a03a933c7c9dcc8cf7f59ab32042bcd99
unknown <nghduc91@gmail.com>
2020-11-02 17:01:13 +0700

    commit 4

--- src/main/java/com/mrd/ProjectQuiz/controller/QuizController.java
+++ src/main/java/com/mrd/ProjectQuiz/controller/QuizController.java
@@ -42,13 +42,13 @@ public class QuizController {
         return new ResponseEntity<>(quiz, HttpStatus.CREATED);
     }
 
-    //trả về danh sách câu hỏi show ra màn hình (1 câu lv1, 1 câu lv2, 1 câu lv3)
+    //trả về danh sách câu hỏi show ra màn hình choi (4 câu lv1, 1 câu lv2, 1 câu lv3) total socre = 10
     @RequestMapping(value = "/api/quiz", method = RequestMethod.GET)
     public ResponseEntity<List<Quiz>> getAllUser() {
         List<Quiz> lstQuizLv1 = quizRepositoryCustom.findByLevel(1);
         List<Quiz> lstQuizLv2 = quizRepositoryCustom.findByLevel(2);
         List<Quiz> lstQuizLv3 = quizRepositoryCustom.findByLevel(3);
-        
+
         //tron cau hoi
         Collections.shuffle(lstQuizLv1);
         Collections.shuffle(lstQuizLv2);
@@ -56,10 +56,12 @@ public class QuizController {
 
         List<Quiz> lstQuiz = new ArrayList<>();
 
-        for (int i = 0; i < 1; i++) {
+        for (int i = 0; i < 4; i++) {
             lstQuiz.add(lstQuizLv1.get(i));
-            lstQuiz.add(lstQuizLv2.get(i));
-            lstQuiz.add(lstQuizLv3.get(i));
+            if (i < 1) {
+                lstQuiz.add(lstQuizLv2.get(i));
+                lstQuiz.add(lstQuizLv3.get(i));
+            }
         }
 
         return new ResponseEntity<>(lstQuiz, HttpStatus.CREATED);
