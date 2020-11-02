a3146616c7c0d0ede91ee528aa25e6c4ab015bea
unknown <nghduc91@gmail.com>
2020-11-02 14:17:40 +0700

    first commit

---
+++ src/main/java/com/mrd/ProjectQuiz/ProjectQuizApplication.java
@@ -0,0 +1,13 @@
+package com.mrd.ProjectQuiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectQuizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectQuizApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mrd/ProjectQuiz/ProjectQuizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mrd.ProjectQuiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectQuizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
